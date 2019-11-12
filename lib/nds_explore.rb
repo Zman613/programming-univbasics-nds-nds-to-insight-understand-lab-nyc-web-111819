718ad141059bd1e3b6cf29828167e3dfdcdd116c
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-12 05:11:44 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ end
 
 def print_first_directors_movie_titles
   i = 0
-  while i < directors_database[0].count do
+  while i < directors_database[0][:movies].count do
     puts "#{directors_database[0][:movies][i][:title]}"
     i += 1
   end
