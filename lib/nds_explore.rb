c7b5119c11070534ac90dc6f28265e535e23d97c
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-12 05:03:31 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ def print_first_directors_movie_titles
   i = 0
   pp directors_database
   while i < directors_database[0].count do
-    print "#{directors_database[0][0][:movies][i][:title]}"
+
     i += 1
   end
 end
